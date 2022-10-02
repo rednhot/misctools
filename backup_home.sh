df251ac916097544d84e9d8a3c8fddff2cfac93e
Matvey <mathway.home@gmail.com>
2022-10-02 03:35:06 +0300

    updated backup script

--- backup_home.sh
+++ backup_home.sh
@@ -2,6 +2,8 @@
 
 # author: mathway
 
+
+# Awkward terminal color constants
 BLACK_FG='\033[01;30m'
 RED_FG='\033[01;31m'
 GREEN_FG='\033[01;32m'
@@ -15,7 +17,24 @@ DEFAULT_FG='\033[00m'
 
 show_help()
 {
-    printf "Usage: $(basename $0) [-e|--encrypt] [-p passphrase] [-P passphrase_file] [-b|--batch] [-c|--color 0|1] [-h|--help] [-v|--verbose] -- [tar_options]\n"
+    local opts
+    declare -A opts # opts now is associative array
+    printf "Usage: $(basename $0) [options...] -- [tar_options...]\n"
+    opts["-c, --color"]="Presense of colors in output. 0 or 1."
+    opts["-v, --verbose"]="Verbose output."
+    opts["-d, --output-dir"]="Directory for backup storing."
+    opts["-h, --help"]="Show this help menu."
+    opts["-o, --output"]="Backup file path."
+    opts["-i, --interactive, --nobatch"]="Be more interactive."
+    opts["-b, --batch"]="Opposite of -i."
+    opts["-t, --telegram"]="Save backup to telegram. !Unimplemented!"
+    opts["-p, --passphrase"]="Passphrase for backup encryption."
+    opts["-P, --passphrase-file"]="Read passphrase from file. If dash(-), read from stdin."
+    opts["-e, --encrypt"]="Encrypt backup using symmetric cipher."
+    for o in "${!opts[@]}"; do
+	printf "\t%-30s\t%s\n" "$o" "${opts[$o]}"
+    done
+    printf "\nArguments after -- are treated as tar(1) options.\n"
 }
 
 maybe_color()
@@ -38,10 +57,17 @@ error()
     exit 1
 }
 
+clean_exit()
+{
+    shred -zfn3 "$tmpf" && rm -f "$tmpf"
+    printf "%s\n" "Goodbye!"
+    exit 0
+}
+
 debug_msg()
 {
     maybe_color "$YELLOW_FG"
-    [ "$LEVEL" -ge 2 ] && { printf "[@] " ; printf "$@" >&2; printf "\n" >&2; }
+    [ "$LEVEL" -ge 2 ] && { printf "[DEBUG] " ; printf "$@" >&2; printf "\n" >&2; }
     reset_color
 }
 
@@ -59,12 +85,12 @@ ENCRYPT=0
 BATCH=0
 COLOR=1
 
-# parameters
+# Options
 backupdir=/tmp
 backupfile=
 passphrase=
 
-
+# Option parsing
 while [ $# -gt 0 ]; do
     case $1 in
 	-c|--color) shift
@@ -73,16 +99,13 @@ while [ $# -gt 0 ]; do
 	-v|--verbose) LEVEL=2
 		      tar_opts="$tar_opts -v"
 		      ;;
-	-d|--output-dir) shift
-			 backupdir=$1
-			 ;;
 	-h|--help) show_help
 		   exit 0
 		   ;;
 	-d|--output-dir) shift
 		      backupdir=$1
 		      ;;
-	-f|--filename) shift
+	-o|--output) shift
 		       backupfile=$1
 		       ;;
 	-i|--interactive|--nobatch) BATCH=0
@@ -97,13 +120,14 @@ while [ $# -gt 0 ]; do
 			 passphrase=$1
 			 ;;
 	-P|--passphrase-file) shift
+			      [ "$1" = "-" ] || 
 			      [ -r "$1" -a -s "$1" ] &&
 				  passphrase="$(cat $1)" ||
 				      error "can't read from file \"$1\""
 			      ;;
 	-e|--encrypt) ENCRYPT=1
 		    ;;
-	--) shift
+	--) shift # Remaining arguments are passed to tar as is.
 	    break
 	    ;;
 	*) printf "Unrecognized option. Use -h for help.\n"
@@ -113,15 +137,12 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-while [ $# -gt 0 ]; do
-    tar_opts="$tar_opts '$1'"
-    shift
-done
-
 if [ -z "$backupfile" ]; then
     backupfile=$backupdir/homebackup_$(date +%d-%m-%Y).tar.gz
 fi
 
+# exit in a clean way if SIGINT is sent
+trap clean_exit SIGINT
 
 { touch "$backupfile" && chmod 600 "$backupfile" ;} || error "Can't create file"
 
@@ -136,9 +157,38 @@ info_msg "Creating archive..."
 cd $HOME
 debug_msg "Backup file is %s" "$backupfile"
 debug_msg "$tar_opts"
-find . -maxdepth 1 -type f -print0 |
-    tar --null --verbatim-files-from -T - --exclude "*~" --exclude '*kali_pentest*' --exclude '*config/discord'  --exclude '*mozilla*extensions' -hczSpf "$tmpf" docs hacking Music pics prog .gnupg .keepassxc .mozilla .config .emacs.d .john .ssh .local/bin $tar_opts 2>/dev/null
+debug_msg "tmpfile is $tmpf"
 
+# Pretty long exclusion list here, but it seems that this is the easier way
+# to maintain rather than whitelisting.
+# Compressing by default.
+
+    
+printf "%s" '
+Desktop 
+Downloads
+QEMU*
+.config/discord 
+.mozilla*extensions
+.wine
+.terminfo
+.face
+.cache
+.Xlog
+.dbus
+hacking/kali*
+.npm
+.gradle
+.java
+.local
+.BurpSuite
+.cargo
+.ropper' | 
+tar -hczSpf "$tmpf" --exclude-from - \
+    --ignore-failed-read --ignore-command-error $tar_opts "$@" \
+    --warning no-file-removed .* *
+
+# Encrypt the backup file if user wants that.
 if [ "$ENCRYPT" = "1" ]; then
     if [ -z "$passphrase" ]; then
 	read -p "Archive passphrase: " passphrase
@@ -152,12 +202,12 @@ if [ "$ENCRYPT" = "1" ]; then
     backupfile="${backupfile}.gpg"
 fi
 
+# Move from temporary location to the user specified location.
 mv "$tmpf" "$backupfile"
 
+# Maybe somewhat paranoid, but why not
 info_msg "Syncing..."
 sync -f "$backupfile"
-
-
 
 info_msg "Back up completed. Happy day/night!"
 exit 0
